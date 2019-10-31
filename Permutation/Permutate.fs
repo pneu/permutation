43620addc55827163a0da7cdf34e092cc2ece9e8
pneu <pneumaster@gmail.com>
2019-10-31 09:08:55 +0900

    separate to files

---
+++ Permutation/Permutate.fs
@@ -0,0 +1,16 @@
+﻿module Permutation
+
+/// <summary>exclude <c>x</c> from <c>xs</c>.</summary>
+let private exclude<'T when 'T: equality> (x: 'T) (xs: 'T list): 'T list = List.filter (fun y -> y <> x) xs
+
+/// <summary>prepend <c>x</c> to all lists of <c>xss</c>.</summary>
+let private prefix (x: 'T) (xss: 'T list list): 'T list list = List.map (fun xs -> x :: xs) xss
+
+let rec private expand<'T when 'T: equality> (x: 'T) (xs: 'T list): 'T list list =
+    permutations (exclude x xs) |> prefix x
+
+and permutations<'T when 'T: equality> (xs: 'T list): 'T list list =
+    match xs with
+    | [ x ] -> [ xs ]
+    | _ ->
+        List.reduce (@) [ for x in xs -> expand x xs ]
--- Permutation/Program.fs
+++ Permutation/Program.fs
@@ -1,19 +1,6 @@
-﻿module Permutation
+module Program
 
-open System
-
-/// <summary>exclude <c>x</c> from <c>xs</c>.</summary>
-let private exclude<'T when 'T: equality> (x: 'T) (xs: 'T list): 'T list = List.filter (fun y -> y <> x) xs
-
-/// <summary>prepend <c>x</c> to all lists of <c>xss</c>.</summary>
-let private prefix (x: 'T) (xss: 'T list list): 'T list list = List.map (fun xs -> x :: xs) xss
-
-let rec private expand<'T when 'T: equality> (x: 'T) (xs: 'T list): 'T list list =
-    permutations (exclude x xs) |> prefix x
-and permutations<'T when 'T: equality> (xs: 'T list): 'T list list =
-    match xs with
-    | [x] -> [xs]
-    | _ -> List.reduce (@) [for x in xs -> expand x xs]
+open Permutation
 
 exception TooFewArgumentsException
 
@@ -24,7 +11,6 @@ let main argv =
         let p = permutations (Array.toList argv)
         printfn "%A" p
         0
-    with
-        | :? TooFewArgumentsException ->
-            eprintfn "Too few Arguments"
-            1
+    with :? TooFewArgumentsException ->
+        eprintfn "Too few Arguments"
+        1
