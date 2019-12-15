832725a80e3198a3c1a9167f7647c4ddfed56cfc
pneu <pneumaster@gmail.com>
2019-12-15 21:40:05 +0900

    seq

--- lib/Permutation/Combination.fs
+++ lib/Permutation/Combination.fs
@@ -1,11 +1,14 @@
 module Combination
 
+open System.Collections.Generic
 
-[<CompiledName "Combinate">]
-let rec combinations (n: int) (list: 'T list): 'T list list =
+let rec combinations (n: int) (list: 'T seq): 'T seq seq =
     match n, list with
-    | 0, _ -> [ [] ]
-    | _, [] -> []
+    | 0, _ -> seq { [] }
+    | _, xs when Seq.isEmpty xs -> Seq.empty
     | k, x :: xs ->
         // Choose x and (k-1) elements from xs, or don't choose x and choose n from xs.
-        (List.map (fun rs -> x :: rs) (combinations (k - 1) xs)) @ combinations k xs
+        (Seq.map (fun rs -> x :: rs) (combinations (k - 1) xs)) @ combinations k xs
+
+let Combinate ((n: int), (list: IEnumerable<'T>)): IEnumerable<IEnumerable<'T>> =
+    combinations n list
